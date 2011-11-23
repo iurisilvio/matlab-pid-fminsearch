0490b2f4fecd04322f7ba03619db65b837e8de49
Iuri de Silvio <iurisilvio@gmail.com>
2011-11-23 03:19:07 -0200

    Removing useless cost files.

--- custo2.m
+++
@@ -1,5 +0,0 @@
-% Sintoniza apenas em relação ao overshoot.
-function [custo] = custo2(ts, overshoot)
-    global erro ref overshoot_spec;
-    custo = sum(min(erro+ref*overshoot_spec,0))^2 + exp(min(erro));
-end
--- custo3.m
+++
@@ -1,6 +0,0 @@
-% Esse custo tenta sintonizar apenas em relação ao tempo de subida.
-% Não funciona.
-function [custo] = custo3(ts, overshoot)
-    global ts_spec;
-    custo = exp(ts - ts_spec);
-end
