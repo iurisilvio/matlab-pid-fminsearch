39530a3bd2a4dbee60b0fbd58f142686fd7b0cfe
Iuri de Silvio <iurisilvio@gmail.com>
2011-11-23 12:45:20 -0200

    Final version.

--- fminsearch_callback.m
+++ fminsearch_callback.m
@@ -1,14 +1,32 @@
 function [fval] = fminsearch_callback(x)
-    global P I D ref % Variaveis compartilhadas com a simulação
-    global ts_spec overshoot_spec step t_max % Variaveis compartilhadas com o callback
+    global A B
+    global P I D ref Yout % Variaveis compartilhadas com a simulação
+    global ts_spec overshoot_spec % Variaveis compartilhadas com o callback
 
-    P = x(1);
-    I = x(2);
-    D = x(3);
+    [rows columns] = size(x);
     
-    [t, state] = sim('simulacao', 0:step:t_max); % Realiza a simulação
-    y = state(:, 5);
+    % Para funcionar a sintonização também com x=[P I D]
+    if columns == 3
+        K = x;
+        P = K(1);
+        I = K(2);
+        D = K(3);
+    elseif columns == 7
+        Q = [x(1) x(2) x(3);
+             x(2) x(4) x(5);
+             x(3) x(5) x(6)];
+        R = x(7);
+        K = lqr(A, B, Q, R);
+        P = -K(1);
+        I = K(2);
+        D = -K(3);
+    else
+        disp('Valor não previsto para "x" na função fminsearch_callback.');
+    end
     
+    sim('simulacao'); % Realiza a simulação
+    y = Yout.signals.values;
+    t = Yout.time;
     overshoot = (max(y) - ref) / ref;
     ts_index = find(y > ref, 1);
     if isempty(ts_index)
@@ -16,6 +34,6 @@ function [fval] = fminsearch_callback(x)
     else
         ts = t(ts_index);
     end
-    fval = abs(overshoot - overshoot_spec) + abs(ts - ts_spec);
-    util = [fval overshoot ts] % Imprime alguns valores úteis
+    fval = abs(overshoot - overshoot_spec) / overshoot_spec + abs(ts - ts_spec) / ts_spec;
+    util = [fval overshoot ts]; % Imprime alguns valores úteis
 end
--- sintonizador.m
+++ sintonizador.m
@@ -2,10 +2,14 @@ clear all
 close all
 warning off
 
+global A B % Matrizes do espaço de estados
 global ref P I D % Variaveis compartilhadas com a simulação
 global ts_spec overshoot_spec step t_max % Variaveis compartilhadas com o callback
 
-step = 0.01; % Passo da simulação
+A = [0 -1 0; -81 0 0; 1 0 0];
+B = [0; 3000; 0];
+
+step = 0.1; % Passo da simulação
 t_max = 6; % Tempo de simulação
 ref = 1; % Referencia
 ts_spec = 1; % Tempo de subida especificado
@@ -13,5 +17,9 @@ overshoot_spec = .1; % Overshoot especificado
 
 K = [1.76 1 1]; % [P I D] inicial
 
+% Q = [a(1) a(2) a(3); a(2) a(4) a(5); a(3) a(5) a(6)]; R = a(7);
+% Q simétrica
+a = [1 0 0 1 0 1 1];
+
 options = optimset('PlotFcns', @optimplotfval);
-Kmin = fminsearch('fminsearch_callback', K, options)
+Kmin = fminsearch('fminsearch_callback', a, options) % É possível alterar 'a' para 'K'
